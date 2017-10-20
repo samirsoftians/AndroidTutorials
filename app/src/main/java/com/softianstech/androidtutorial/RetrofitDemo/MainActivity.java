55086f6340c862bf655b7af4dd658b8539629ced
samirsoftians <samir.softians@gmail.com>
2017-10-20 17:03:34 +0530

    Second Project Added Reccler View

---
+++ app/src/main/java/com/softianstech/androidtutorial/RetrofitDemo/MainActivity.java
@@ -0,0 +1,73 @@
+package com.softianstech.androidtutorial.RetrofitDemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.softianstech.androidtutorial.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Lenovo on 10/20/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    //a list to store all the products
+    List<Product> productList;
+
+    //the recyclerview
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //getting the recyclerview from xml
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //initializing the productlist
+        productList = new ArrayList<>();
+
+
+        //adding some items to our list
+        productList.add(
+                new Product(
+                        1,
+                        "Apple MacBook Air Core i5 5th Gen - (8 GB/128 GB SSD/Mac OS Sierra)",
+                        "13.3 inch, Silver, 1.35 kg",
+                        4.3,
+                        60000,
+                        R.drawable.macbook));
+
+        productList.add(
+                new Product(
+                        1,
+                        "Dell Inspiron 7000 Core i5 7th Gen - (8 GB/1 TB HDD/Windows 10 Home)",
+                        "14 inch, Gray, 1.659 kg",
+                        4.3,
+                        60000,
+                        R.drawable.dellinspiron));
+
+        productList.add(
+                new Product(
+                        1,
+                        "Microsoft Surface Pro 4 Core m3 6th Gen - (4 GB/128 GB SSD/Windows 10)",
+                        "13.3 inch, Silver, 1.35 kg",
+                        4.3,
+                        60000,
+                        R.drawable.surface));
+
+        //creating recyclerview adapter
+        ProductAdapter adapter = new ProductAdapter(this, productList);
+
+        //setting adapter to recyclerview
+        recyclerView.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/softianstech/androidtutorial/RetrofitDemo/ProductAdapter.java
@@ -0,0 +1,79 @@
+package com.softianstech.androidtutorial.RetrofitDemo;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.softianstech.androidtutorial.R;
+
+import java.util.List;
+
+/**
+ * Created by Lenovo on 10/20/2017.
+ */
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
+
+
+    //this context we will use to inflate the layout
+    private Context mCtx;
+
+    //we are storing all the products in a list
+    private List<Product> productList;
+
+    //getting the context and product list with constructor
+    public ProductAdapter(Context mCtx, List<Product> productList) {
+        this.mCtx = mCtx;
+        this.productList = productList;
+    }
+
+    @Override
+    public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //inflating and returning our view holder
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view = inflater.inflate(R.layout.layout_products, null);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ProductViewHolder holder, int position) {
+        //getting the product of the specified position
+        Product product = productList.get(position);
+
+        //binding the data with the viewholder views
+        holder.textViewTitle.setText(product.getTitle());
+        holder.textViewShortDesc.setText(product.getShortdesc());
+        holder.textViewRating.setText(String.valueOf(product.getRating()));
+        holder.textViewPrice.setText(String.valueOf(product.getPrice()));
+
+        holder.imageView.setImageDrawable(mCtx.getResources().getDrawable(product.getImage()));
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+
+    class ProductViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewTitle, textViewShortDesc, textViewRating, textViewPrice;
+        ImageView imageView;
+
+        public ProductViewHolder(View itemView) {
+            super(itemView);
+
+            textViewTitle = (TextView) itemView.findViewById(R.id.textViewTitle);
+            textViewShortDesc = (TextView) itemView.findViewById(R.id.textViewShortDesc);
+            textViewRating = (TextView) itemView.findViewById(R.id.textViewRating);
+            textViewPrice = (TextView) itemView.findViewById(R.id.textViewPrice);
+            imageView = (ImageView) itemView.findViewById(R.id.imageView);
+        }
+    }
+}
